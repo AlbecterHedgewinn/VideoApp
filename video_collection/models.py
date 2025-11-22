3626ac7e44befecf93bc9c6b544789ed45ae97e9
danielpbittner <fakeemail@gmail.com>
2025-11-22 16:20:17 -0600

    added video list html, url path, view, and model. Also fixed looping dependency issue with an erroneous import

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from urllib import parse
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -6,6 +8,26 @@ class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True) # enforcing unique id numbers
+
+    def save(self, *args, **kwargs):
+        # extract the video id from a youtube url
+        if not self.url.startswith('https://www.youtube.com/watch'):
+            raise ValidationError(f'Not a YouTube video: {self.url}')
+
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query # 'v=12345678'
+
+        if not query_string:
+            raise ValidationError(f'Invalid YouTube URL {self.url}')
+        parameters = parse.parse_qs(query_string, strict_parsing=True)  # dictionary
+        v_parameters_list = parameters.get('v')                         # return None if no key found, e.g abc=1234&abc=12345678
+
+        if not v_parameters_list:                                       # checking if None or empty list
+            raise ValidationError(f'Invalid YouTube URL, missing parameters {self.url}') 
+        self.video_id = v_parameters_list [0] # string
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, VideoID: {self.video_id}, Notes: {self.notes[:200]}'
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from .views import VideoForm
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from .forms import VideoForm
+from .models import Video # Lets us query the database
 
 # Create your views here.
 
@@ -15,12 +18,21 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'New Video Saved!')
-            # TODO show success message or redirect
-        else:
-            messages.warning(request, 'Please check the data entered.')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                #   messages.info(request, 'New Video Saved!') # show success message
+            except ValidationError:                         # Catch for incorrect url
+                messages.warning(request, 'Invalid YouTube Url')
+            except IntegrityError:                          # catch for potential copies in database
+                messages.warning(request, 'You already added this video.')
+        
+        messages.warning(request, 'Please check the data entered.')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
